75a126a8f451954ed29316a06f31b18dd00e983f
peanutenthusiast <mhdedios@gmail.com>
2017-10-08 10:12:04 -0700

    Render table of transactions

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -2,13 +2,35 @@ import React, { Component } from 'react';
 
 
 class TransactionsList extends Component {
-  render() {
-    return (
-      <div className="TransactionsList">
 
+
+  render() {
+
+    return(
+      <div className="TransactionsList">
+        <table>
+          <tbody>
+            <tr>
+              <th>Date</th>
+              <th>Description</th>
+              <th>Amount</th>
+            </tr>
+            {this.props.transactionsList.map(transaction => {
+                return(
+                <tr>
+                  <td>{transaction.date_time.slice(0, 10)}</td>
+                  <td>{transaction.description.slice(0, 15)}</td>
+                  <td>{transaction.posted_amount}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
       </div>
     );
   }
+
 }
 
 export default TransactionsList;
+
