5645dcb31968a2e17fd7abdfa455bbd30321a68a
Michael Glaser <m.glaser82@gmail.com>
2017-10-08 05:09:34 -0700

    Adds highchart component to the CurrentWeeklyData component.

--- src/components/CurrentWeeklyData.js
+++ src/components/CurrentWeeklyData.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import Highcharts from 'highcharts';
+// var Highcharts = require('highcharts');
+// require('highcharts/modules/funnel')(Highcharts);
 
 class CurrentWeeklyData extends Component {
+
+    componentDidMount() {
+       if (this.props.modules) {
+           this.props.modules.forEach(function (module) {
+               module(Highcharts);
+           });
+       }
+       // Set container which the chart should render to.
+       this.chart = new Highcharts[this.props.type || "Chart"](
+           this.props.container,
+           this.props.options
+       );
+   }
+   //Destroy chart before unmount.
+   componentWillUnmount() {
+       this.chart.destroy();
+   }
+
   render() {
     return (
       <div className="CurrentWeeklyData">
-        turd
+      	{this.props.container}
       </div>
     );
   }
