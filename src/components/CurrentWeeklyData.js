d8c343f1bb9158e98eae9059736d16e8b7a3a5cf
peanutenthusiast <mhdedios@gmail.com>
2017-10-07 17:50:04 -0700

    Add skeleton subcomponents and header

---
+++ src/components/CurrentWeeklyData.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class CurrentWeeklyData extends Component {
+  render() {
+    return (
+      <div className="CurrentWeeklyData">
+
+      </div>
+    );
+  }
+}
+
+export default CurrentWeeklyData;
---
+++ src/components/TransactionsList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class TransactionsList extends Component {
+  render() {
+    return (
+      <div className="TransactionsList">
+
+      </div>
+    );
+  }
+}
+
+export default TransactionsList;
