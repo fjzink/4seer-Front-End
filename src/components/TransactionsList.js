1acba42fd459d83d98b6ec09af52c03f886f641b
jnickell <nickell75@yahoo.com>
2017-10-08 10:10:46 -0700

    Adds css elements to front-end, start work on budget component

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-
+import styles from '../App.css';
 
 class TransactionsList extends Component {
   render() {
     return (
       <div className="TransactionsList">
-
+      		<h3>Current Monthly Budget</h3>
+      		<div>
+      			chart
+      		</div>
       </div>
     );
   }
