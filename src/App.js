a199e2cc7ee852f8a9a6c798e684115b498ce3dc
peanutenthusiast <mhdedios@gmail.com>
2017-10-08 04:34:35 -0700

    Add axios to package.json, add constructor and state variables to app.js, add prototype axios call to back end, add componentdidmount lifecycle function

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import CurrentMonthBudget from './components/CurrentMonthBudget.js';
 import HistoricalData from './components/HistoricalData.js';
 import CurrentWeeklyData from './components/CurrentWeeklyData.js';
 import TransactionsList from './components/TransactionsList.js';
+import axios from 'axios';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      accounts: "",
+      transactionsList: []
+    }
+    this.loadData = this.loadData.bind(this);
+  }
 
+  loadData() {
+    axios.get('http://localhost:3000/api/accounts/1').then(response => {
+      console.log(response);
+      debugger
+    }).catch(errors => console.log(errors));
+  }
+
+  componentDidMount() {
+    this.loadData();
   }
 
   render() {
