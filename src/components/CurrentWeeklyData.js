103e5c275201832e7c4acdf0d2f0563b6cb7bb3f
Michael Glaser <m.glaser82@gmail.com>
2017-10-08 09:37:49 -0700

    Add functionality to chart data to component and plot to screen.

--- src/components/CurrentWeeklyData.js
+++ src/components/CurrentWeeklyData.js
@@ -1,33 +1,58 @@
 import React, { Component } from 'react';
-import Highcharts from 'highcharts';
-// var Highcharts = require('highcharts');
-// require('highcharts/modules/funnel')(Highcharts);
+import ReactJSXHighcharts, { HighchartsChart, Chart, Title, Subtitle, Legend, XAxis, YAxis, LineSeries } from 'react-jsx-highcharts';
+import ReactDOM from 'react-dom';
 
 class CurrentWeeklyData extends Component {
 
-    componentDidMount() {
-       if (this.props.modules) {
-           this.props.modules.forEach(function (module) {
-               module(Highcharts);
-           });
-       }
-       // Set container which the chart should render to.
-       this.chart = new Highcharts[this.props.type || "Chart"](
-           this.props.container,
-           this.props.options
-       );
-   }
-   //Destroy chart before unmount.
-   componentWillUnmount() {
-       this.chart.destroy();
-   }
+   //  componentDidMount() {
+   //     if (this.props.modules) {
+   //         this.props.modules.forEach(function (module) {
+   //             module(Highcharts);
+   //         });
+   //     }
+   //     // Set container which the chart should render to.
+   //     this.chart = new Highcharts[this.props.type || "Chart"](
+   //         this.props.container,
+   //         this.props.options
+   //     );
+   // }
+   // //Destroy chart before unmount.
+   // componentWillUnmount() {
+   //     this.chart.destroy();
+   // }
+   
 
   render() {
-    return (
-      <div className="CurrentWeeklyData">
-      	{this.props.container}
-      </div>
-    );
+    const plotOptions = {
+    series: {
+      pointStart: 2010
+    }
+  };
+
+  return (
+    <HighchartsChart plotOptions={plotOptions}>
+      <Chart />
+
+      <Title>Solar Employment Growth by Sector, 2010-2016</Title>
+
+      <Subtitle>Source: thesolarfoundation.com</Subtitle>
+
+      <Legend layout="vertical" align="right" verticalAlign="middle" />
+
+      <XAxis>
+        <XAxis.Title>Time</XAxis.Title>
+      </XAxis>
+
+      <YAxis id="number">
+        <YAxis.Title>Number of employees</YAxis.Title>
+        <LineSeries id="installation" name="Installation" data={[43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]} />
+        <LineSeries id="manufacturing" name="Manufacturing" data={[24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]} />
+        <LineSeries id="sales-distribution" name="Sales & Distribution" data={[11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]} />
+        <LineSeries id="project-development" name="Project Development" data={[null, null, 7988, 12169, 15112, 22452, 34400, 34227]} />
+        <LineSeries id="other" name="Other" data={[12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]} />
+      </YAxis>
+    </HighchartsChart>
+  );
   }
 }
 
